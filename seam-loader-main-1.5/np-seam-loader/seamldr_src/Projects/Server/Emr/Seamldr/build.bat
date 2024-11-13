8ee710b3e7e4ed520aa16dd21debea2d08e4a3c8
Jaewon Hur <hurjaewon@snu.ac.kr>
2024-11-13 05:11:29 +0000

    built.bat

---
+++ seam-loader-main-1.5/np-seam-loader/seamldr_src/Projects/Server/Emr/Seamldr/build.bat
@@ -0,0 +1,7 @@
+set input=%1
+make -f ..\project.mak npseamldr cleanall
+git checkout .
+scp mundo:~/p-seam-loader/bin/debug/pseamldr.so ./
+scp mundo:~/p-seam-loader/bin/debug/pseamldr.so.consts ./
+make -f ..\porject.mak npseamldr
+scp output\ENG_TR_O1\EMR_NP_SEAMLDR_ENG_TR_O1.DBG.bin mundo:~/bin/%input%.bin
