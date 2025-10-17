372ac2ea710d0c45957ecc65126ccb1f21612f8e
Jaewon Hur <hurjaewon@snu.ac.kr>
2025-10-17 15:01:09 -0400

    Fix build scripts
    Signed-off-by: Jaewon Hur <hurjaewon@snu.ac.kr>

--- seam-loader-main-1.5/np-seam-loader/seamldr_src/Projects/Server/Emr/Seamldr/build.bat
+++ seam-loader-main-1.5/np-seam-loader/seamldr_src/Projects/Server/Emr/Seamldr/build.bat
@@ -1,7 +1,20 @@
-set input=%1
+@echo off
+setlocal
+
+:: Check for pseamldr.so and pseamldr.so.consts
+if not exist pseamldr.so (
+    echo Please build pseamldr.so and pseamldr.so.consts and place them under this directory.
+    echo You can build them by running 'build.sh' in 'seam-loader\seam-loader-main-1.5\p-seam-loader' on Ubuntu.
+    exit /b 1
+)
+if not exist pseamldr.so.consts (
+    echo Please build pseamldr.so and pseamldr.so.consts and place them under this directory.
+    echo You can build them by running 'build.sh' in 'seam-loader\seam-loader-main-1.5\p-seam-loader' on Ubuntu.
+    exit /b 1
+)
+
 make -f ..\project.mak npseamldr cleanall
 git checkout .
-scp mundo:~/p-seam-loader/bin/debug/pseamldr.so ./
-scp mundo:~/p-seam-loader/bin/debug/pseamldr.so.consts ./
 make -f ..\project.mak npseamldr
-scp output\ENG_TR_O1\EMR_NP_SEAMLDR_ENG_TR_O1.DBG.bin mundo:~/bin/%input%.bin
+
+echo NP-SEAMLDR binary built under 'output\ENG_TR_O1\EMR_NP_SEAMLDR_ENG_TR_O1.DBG.bin'.
